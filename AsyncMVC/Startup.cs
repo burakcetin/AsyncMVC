7ef8791f69cfcd0ecd0fda457edd7b9115fa5f8f
Burak Cetin <bc@some.net>
2014-05-05 14:04:43 +0300

    first

---
+++ AsyncMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AsyncMVC.Startup))]
+namespace AsyncMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
